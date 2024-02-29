c66543a5d040cf239fc3bc747675138bf365ed97
Parisa Moshfegh <paria340@gmail.com>
2024-02-28 22:17:15 -0800

    app connected to mongoose locally

--- db/config.js
+++ db/config.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/bahar-cafe")
+  .connect("mongodb://127.0.0.1:27017/baharCafe")
   .catch((error) => console.log(error));
-
 // const uri = "mongodb+srv://parisa:Paris1375@cluster0.bzhv7.mongodb.net/";
 // const client = new MongoClient(uri);
 // client.connect();
--- index.js
+++ index.js
@@ -14,45 +14,4 @@ app.post("/register", async (req, resp) => {
     resp.send(req.body)
 })
 app.listen(5000)
-//
-// const mongoose = require('mongoose');
-// const app = express();
-// const connectDB = async()=> {
-//     mongoose.connect('mongodb://localhost:27017/baharCafe');
-//     const productSchema = new mongoose.Schema({});
-//     const product = mongoose.model("products", productSchema);
-//     const data =  await product.find();
-//     console.warn(data);
-// }
 
-// connectDB();
-
-
-
-// const { MongoClient } = require("mongodb");
-
-// // Replace the uri string with your connection string.
-// const uri =
-//   "mongodb+srv://parisa:Paris1375@cluster0.bzhv7.mongodb.net/";
-// const client = new MongoClient(uri);
-
-// async function run() {
-//   try {
-//     await client.connect();
-
-//     const database = client.db('baharCafe');
-//     const movies = database.collection('products');
-//     // console.log(database);
-//     // console.log('break')
-
-//     // Query for a movie that has the title 'Back to the Future'
-//     const query = { name: 'Marble Cake' };
-//     const movie = await movies.findOne(query);
-
-//     console.log(movie);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
